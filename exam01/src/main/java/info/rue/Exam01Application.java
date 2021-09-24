2426a286021cf77da8f30f7e8e7fa591dda5980e
repoetry <repoetry@hotmail.com>
2021-09-25 02:00:07 +0900

    DataBase 설정 제외. @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)

--- exam01/src/main/java/info/rue/Exam01Application.java
+++ exam01/src/main/java/info/rue/Exam01Application.java
@@ -2,8 +2,9 @@ package info.rue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class Exam01Application {
 
     public static void main(String[] args) {
