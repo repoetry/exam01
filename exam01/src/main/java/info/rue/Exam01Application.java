482f1d2d6929e31f8c37ad80c7af00a4f52c9aff
repoetry <repoetry@hotmail.com>
2021-09-29 23:34:41 +0900

    오라클 클라우드 DB 설정

--- exam01/src/main/java/info/rue/Exam01Application.java
+++ exam01/src/main/java/info/rue/Exam01Application.java
@@ -2,9 +2,8 @@ package info.rue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class Exam01Application {
 
     public static void main(String[] args) {
